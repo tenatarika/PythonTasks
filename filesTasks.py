f4733c4ca0704330f30ccca11eccaa365220a984
Furcas <tenatarika@gmail.com>
2021-04-13 14:15:36 +0300

    added new comments

--- filesTasks.py
+++ filesTasks.py
@@ -6,6 +6,22 @@ Created on Sat Apr 10 00:53:48 2021
 """
 
 def task1(fileName, numofLines):
+    '''
+    
+
+    Parameters
+    ----------
+    fileName : TYPE
+        DESCRIPTION.
+    numofLines : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    ans : TYPE
+        DESCRIPTION.
+
+    '''
     f = open(str(fileName), 'r'   )  
     ans = []
     for i in range(numofLines):
@@ -18,6 +34,24 @@ def task1(fileName, numofLines):
 
 
 def task2(fileName, N1, N2):
+    '''
+    
+
+    Parameters
+    ----------
+    fileName : TYPE
+        DESCRIPTION.
+    N1 : TYPE
+        DESCRIPTION.
+    N2 : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
+
+    '''
     with open(str(fileName), 'r') as f:
         return f.read()[N1:N2]
 
@@ -25,6 +59,20 @@ def task2(fileName, N1, N2):
 
 
 def task3(fileName):
+    '''
+    
+
+    Parameters
+    ----------
+    fileName : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
+
+    '''
     f = open(fileName, "r")
     words = f.read().split();
     
@@ -36,9 +84,24 @@ def task3(fileName):
     
 
 
-print(task3("Sutnic.txt"))
+#(task3("Sutnic.txt"))
 
 def task4(fileName, listOfWords):
+    '''
+    
+
+    Parameters
+    ----------
+    fileName : TYPE
+        DESCRIPTION.
+    listOfWords : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    '''
     with open(str(fileName), 'w') as f:
         for word in listOfWords:
             
@@ -51,6 +114,22 @@ def task4(fileName, listOfWords):
 #print(task4("Sutnic.txt", ['Aliens', 'BladeRunner', 'Pi', '1=1', 'oldboy']))
 
 def task5(fileName, sWord):
+    '''
+    
+
+    Parameters
+    ----------
+    fileName : TYPE
+        DESCRIPTION.
+    sWord : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    bool
+        DESCRIPTION.
+
+    '''
     
     with open(str(fileName), 'r') as f:
         words = f.read().split()
@@ -67,6 +146,20 @@ def task5(fileName, sWord):
 
 
 def task6(fileName):
+    '''
+    
+
+    Parameters
+    ----------
+    fileName : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    namesDict : TYPE
+        DESCRIPTION.
+
+    '''
     
     with open(str(fileName), 'r') as f:
         words = f.read().split()
@@ -86,6 +179,20 @@ def task6(fileName):
    
 
 def task7(s):
+    '''
+    
+
+    Parameters
+    ----------
+    s : str
+        DESCRIPTION.
+
+    Returns
+    -------
+    bool
+        DESCRIPTION.
+
+    '''
     s = s.lower()
     if s.replace(' ','')[::-1] == s.replace(' ',''):
         return True
@@ -95,6 +202,20 @@ def task7(s):
 #print(task7("Able was I ere I saw Elba"))
 
 def task8(s):
+    '''
+    
+
+    Parameters
+    ----------
+    s : str
+        DESCRIPTION.
+
+    Returns
+    -------
+    dict
+        DESCRIPTION.
+
+    '''
     u = 0
     l = 0
     for item in s:        
