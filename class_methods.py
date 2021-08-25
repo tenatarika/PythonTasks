2ef7de377a475ebbdfc546adbaee452ed6432bb4
Furcas <tenatarika@gmail.com>
2021-08-26 00:15:46 +0300

    add prim to cls slf methods

--- class_methods.py
+++ class_methods.py
@@ -21,4 +21,21 @@ print(obj.static_method())
 
 print(Foo.class_method())
 print(Foo.static_method())
-print(Foo.method())
+# print(Foo.method()) don't work  
+
+
+class Book():
+    def __init__(self, name : str, price : float, author : str) :
+        self.name = name
+        self.price = price
+        self.author = author
+        
+    def __repr__(self) -> str:
+        return "Book {self.name} by {self.author} for {self.price}"
+    
+    def cost_with_tax(self):
+        return self.tax(self.price)
+    
+    @staticmethod
+    def tax(price: int):
+        return price*1.13
